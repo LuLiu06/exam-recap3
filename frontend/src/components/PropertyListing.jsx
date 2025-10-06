0bbd38c61e0510bb4f3ad471c06e81da43739d87
lulu <krystalanyong@gmail.com>
2025-10-06 11:34:52 +0300

    modified fronted pages

---
+++ frontend/src/components/PropertyListing.jsx
@@ -0,0 +1,15 @@
+const PropertyListing = ({ property }) => {
+  return (
+    <div className="property-preview">
+      <h2>{property.title}</h2>
+      <p>Type: {property.type}</p>
+      <p>Description: {property.description}</p>
+      <p>Price: ${property.price}</p>
+      <p>Location: {property.location.address}, {property.location.city}, {property.location.state} {property.location.zipCode}</p>
+      <p>Square Feet: {property.squareFeet}</p>
+      <p>Year Built: {property.yearBuilt}</p>
+    </div>
+  );
+};
+
+export default PropertyListing;
---
+++ frontend/src/components/PropertyListings.jsx
@@ -0,0 +1,13 @@
+import PropertyListing from "./PropertyListing";
+
+const PropertyListings = ({ properties }) => {
+  return (
+    <div className="property-list">
+      {properties.map((property) => (
+        <PropertyListing key={property.id} property={property} />
+      ))}
+    </div>
+  );
+};
+
+export default PropertyListings;
